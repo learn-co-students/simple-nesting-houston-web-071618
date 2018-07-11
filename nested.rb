f8a46c30a605b1c47028040ef1083498c9821c5e
Scott Ungchusri <animoogle504@gmail.com>
2018-07-11 18:53:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  my_hash = programmer_hash
+  my_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     
+     my_hash = programmer_hash
+     my_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     my_hash = programmer_hash
+     my_hash[:dennis_ritchie][:languages].to_a[0]
 end
 
 def adding_matz
@@ -80,7 +88,11 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",    
+    :languages => ["LISP", "C"]
+    }
+  programmer_hash
 end
 
 def changing_alan
@@ -101,6 +113,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
@@ -121,5 +135,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages].to_a.push("Assembly")
+  programmer_hash
 end
